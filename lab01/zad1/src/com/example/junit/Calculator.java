8d67dc62822d90a64b4dcd85dff079a7a52b9857
Kombi92 <92.konrad@gmail.com>
2016-02-15 13:33:51 +0100

    Added Tests

---
+++ lab01/zad1/src/com/example/junit/Calculator.java
@@ -0,0 +1,39 @@
+package com.example.junit;
+
+/**
+ * Created by Konrad on 15.02.2016.
+ */
+public class Calculator {
+
+    public Calculator() {
+        System.out.println("Calculator: " + this + '\n');
+    }
+
+    public int add(int n1, int  n2){
+
+        return n1 + n2;
+    }
+
+    public int sub(int n1,int  n2){
+
+        return n1 - n2;
+    }
+
+    public int multi(int n1,int  n2){
+
+        return n1 * n2;
+    }
+
+    public int div(int n1,int  n2){
+
+        return n1 / n2;
+    }
+
+    public boolean greater(int n1,int  n2){
+
+        if(n1>n2)
+            return true;
+        else
+            return false;
+    }
+}
