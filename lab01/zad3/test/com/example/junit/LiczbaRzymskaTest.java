1736d184e61e1b5993ced9016667ec6421ccc284
Kombi92 <92.konrad@gmail.com>
2016-02-22 13:23:45 +0100

    Added toString tests

--- lab01/zad3/test/com/example/junit/LiczbaRzymskaTest.java
+++ lab01/zad3/test/com/example/junit/LiczbaRzymskaTest.java
@@ -1,18 +1,30 @@
 package com.example.junit;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Konrad on 15.02.2016.
  */
 public class LiczbaRzymskaTest {
 
-    int liczba =  LiczbaRzymska(10) ;
 
     @Test
-    public void chceck(){
+    public void chceckRoman(){
 
-        assertEquals("x")
+        assertEquals(LiczbaRzymska.toString(10) , "X" );
+        assertEquals(LiczbaRzymska.toString(11) , "XI" );
+        assertEquals(LiczbaRzymska.toString(4) , "IV" );
+        assertEquals(LiczbaRzymska.toString(1410) , "MCDX" );
+
+    }
+
+    @Test
+    public void chceckRange(){
+
+        assertEquals(LiczbaRzymska.toString(0) , "Przekroczono zakres." );
+        assertEquals(LiczbaRzymska.toString(-3) , "Przekroczono zakres." );
+        assertEquals(LiczbaRzymska.toString(5000) , "Przekroczono zakres." );
 
     }
 
