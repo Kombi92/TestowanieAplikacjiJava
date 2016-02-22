b0595cd6db86cb3744126ecb0c90a7a2870f9928
Kombi92 <92.konrad@gmail.com>
2016-02-22 15:55:21 +0100

    Made Mvn projects

---
+++ lab01/zad3/src/test/java/LiczbaRzymskaTest.java
@@ -0,0 +1,31 @@
+import com.example.junit.LiczbaRzymska;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Konrad on 15.02.2016.
+ */
+public class LiczbaRzymskaTest {
+
+
+    @Test
+    public void chceckRoman(){
+
+        Assert.assertEquals(LiczbaRzymska.toString(10) , "X" );
+        assertEquals(LiczbaRzymska.toString(11) , "XI" );
+        assertEquals(LiczbaRzymska.toString(4) , "IV" );
+        assertEquals(LiczbaRzymska.toString(1410) , "MCDX" );
+
+    }
+
+    @Test
+    public void chceckRange(){
+
+        assertEquals(LiczbaRzymska.toString(0) , "Przekroczono zakres." );
+        assertEquals(LiczbaRzymska.toString(-3) , "Przekroczono zakres." );
+        assertEquals(LiczbaRzymska.toString(5000) , "Przekroczono zakres." );
+
+    }
+
+}
